0942e825a664045efbbb74b7ba6fc49a83afb2df
JOSH ARNOLD <jarnold@ucdavis.edu>
2020-03-31 11:11:38 -0700

    intial commit

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # GauGanAPI
+
+## Description
+
+A light-weight API for bulk processing images to `http://nvidia-research-mingyuliu.com/gaugan/`.
+
+## Installation
+
+### Environment
+
+You first need to install the requirements. Run:
+
+`pip3 install -r requirements.txt`
+
+Or, optionally, create your own virtual environment by:
+
+```python
+pip3 install virtualenv
+virtualenv env
+source env/bin/activate
+pip install -r requirements.txt
+```
+
+### Chrome Driver
+
+1. You must have Google Chrome installed.
+
+2. You have to check your Google Chrome version by doing `Chrome > About Google Chrome`. You can see in the image below I am running Google Chrome version 80. If you're on a terminal, you'll have to google another way to figure out your chrome version.
+
+![alt text](https://ibb.co/1dk4gtP)
+
+3. The last step is you need the `chromedriver` for your version of Google Chrome. Jump to `https://sites.google.com/a/chromium.org/chromedriver/downloads` and download whatever version of chrome driver that corresponds to your version of Google Chrome. For me, I would download version 80. Lastly, unzip the downloaded folder and put "chromedriver" in the root of this Github folder.
+
+![alt text](https://ibb.co/zPd2sPR)
+
+## Running the code
+
+### Where to put your files
+
+All you have to do is place any folders or files under `input/`. The code uses `glob`, so you don't have to worry about "flattening" your input folders, as long as each image has a unique name, it doesn't matter how many folders are within your `input/` folder. So, for example, both `input/<my images>` would work and also `input/another_folder/<my images>`.
+
+### The API
+
+The function below initalizes the API. `use_display` is specific to Linux, if you use Linux, you want to try to set this to `True`. If you run into any issues, you could try setting it to `False` and seeing if it works. `headless` determines whether or not you want to "see" the web browser pop up when you run the code. Most of the time, you probably won't so I'd recommend setting it to `True` to hide the web browser. `width` and `height` determines the size of your webbrowser, the following values `1920` and `1080` should be fine.
+
+```python
+upload = Upload(use_display=False, headless=True, width=1920, height=1080)
+```
+
+The function below actually handles uploading all the files. `file_types` is an array that uses enums. Simply pass in `FileType.png` for `png` input files, and `FileType.jpg` for `jpg` input files, or both if you want both file types to be uploaded. `sleep_interval` is pretty important, if it is too small, the API may not have time to load and you won't get any output files. I have found `1` works fine, but if you run into any issues, try to increase it. It should take ~`2 * sleep_interval` to process one image.
+
+```python
+upload.all(file_types=[FileType.png, FileType.jpg], sleep_interval=1)
+```
---
+++ upload.py
@@ -0,0 +1,72 @@
+from pyvirtualdisplay import Display
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from time import sleep
+from enum import Enum
+from glob import glob
+import os
+import base64
+from PIL import Image
+from io import BytesIO
+
+class FileType(Enum):
+    png = 1
+    jpg = 2
+
+class Upload:
+
+    def __init__(self, use_display=False, headless=True, width=1920, height=1080):
+        if use_display:
+            self.display = Display(visible=0, size=(4000, 2500))  
+            self.display.start()
+        self.chrome_options = Options()
+        self.chrome_options.add_argument(f"--window-size={width},{height}")
+        if headless:
+            self.chrome_options.add_argument("--headless")
+        self.driver = webdriver.Chrome('chromedriver', options=self.chrome_options)                        
+        self.driver.get("http://34.216.122.111/gaugan/")
+        self.driver.find_element_by_id("myCheck").click()
+
+    def all(self, file_types=[FileType.png, FileType.jpg], sleep_interval=1):
+        input_file_paths = self.get_input_file_paths(file_types)
+        for input_path in input_file_paths:
+            self.upload_single_file(input_path)
+            sleep(sleep_interval)
+            self.download_output(input_path)
+            sleep(sleep_interval)
+        self.close()
+
+    def get_input_file_paths(self, file_types=[FileType.png, FileType.jpg]):
+        result = []        
+        input_path = os.path.join(os.getcwd(), 'input/')
+        if FileType.png in file_types:
+            result.extend(glob(f"{input_path}*.png"))
+        if FileType.jpg in file_types:
+            result.extend(glob(f"{input_path}*.jpg"))
+        return result
+
+    def upload_single_file(self, input_path):
+        self.driver.find_element_by_id('segmapfile').send_keys(input_path)
+        self.driver.find_element_by_id('btnSegmapLoad').click()        
+        self.driver.find_element_by_id("render").click()
+
+    def download_output(self, input_path):
+        output_path = input_path.replace("/input/", "/output/")
+        canvas = self.driver.find_element_by_id('output')
+        canvas_base64 = self.driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);", canvas)        
+        self.canvas_png = base64.b64decode(canvas_base64)
+        with open(output_path, 'wb') as f: f.write(self.canvas_png)
+
+    def image_data(self):                    
+        image = Image.open(BytesIO(self.canvas_png))
+        buffered = BytesIO()
+        image.save(buffered, format="png")                
+        return 'data:image/png;base64,'+base64.b64encode(buffered.getvalue()).decode('ascii')    
+
+    def close(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    upload = Upload(use_display=False, headless=True, width=1920, height=1080)
+    upload.all(file_types=[FileType.png, FileType.jpg], sleep_interval=1)
+    print("[DONE]")
